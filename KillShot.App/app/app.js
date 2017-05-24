a47aab71be7fc7cb5a7109cf7ee8229517e13319
Alex Saber <alexsaber89@gmail.com>
2017-05-23 21:14:09 -0500

    angular boilerplate; set initial routes

--- KillShot.App/app/app.js
+++ KillShot.App/app/app.js
@@ -1,1 +1,28 @@
-﻿
+﻿var app = angular.module("KillShot", ["ngRoute"]);
+
+app.config([
+    "$routeProvider", function ($routeProvider) {
+        $routeProvider
+            .when("/",
+            {
+                templateUrl: "app/partials/Login.html",
+                controller: "loginController"
+            })
+            .when("/home",
+            {
+                templateUrl: "app/partials/Home.html",
+                controller: "homeController"
+            });
+
+    }
+])
+
+app.run(["$http", function ($http) {
+
+    var token = sessionStorage.getItem('token');
+
+    if (token)
+        $http.defaults.headers.common['Authorization'] = `bearer ${token}`;
+
+    }
+]);
